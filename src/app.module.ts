f91ad08072a87fc02205ebfe6b7d370885ba4150
Ron Almog <ron8132@gmail.com>
2022-12-18 22:37:58 -0500

    add datasource, update app.module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,19 +3,11 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatModule } from './cat/cat.module';
+import { dataSourceOptions } from '../db/datasource';
 import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [
-    CatModule,
-    TypeOrmModule.forRoot({
-      type: 'sqlite',
-      database: 'db1.sqlite',
-      entities: ['dist/**/*.entity.js'],
-      synchronize: true,
-    }),
-    UsersModule,
-  ],
+  imports: [CatModule, TypeOrmModule.forRoot(dataSourceOptions), UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
